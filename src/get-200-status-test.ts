6df6106ded514d3a48af63bf2597d1f84878aceb
TapanGL1 <tapan.pathy@globallogic.com>
2025-03-04 17:45:24 +0530

    Update get-200-status-test.ts

--- src/get-200-status-test.ts
+++ src/get-200-status-test.ts
@@ -3,12 +3,19 @@ import { Options } from 'k6/options';
 import http from 'k6/http';
 
 export let options:Options = {
-  vus: 50,
-  duration: '10s'
+  vus: 1,
+  duration: '30s'
 };
-
+const session = new Httpx();
 export default () => {
   const res = http.get('https://cdn.segment.com/v1/projects/D85a8FDQrDeMzkihHnm5r2ulc2APP2vt/settings');
+  session.addHeaders({
+  'accept': '*/*',
+  'accept-encoding': 'gzip, deflate, br, zstd',
+  'Content-Type': 'application/json',
+  'referer': 'https://app-stage.pluralsight.com/library/',
+  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+});
   check(res, {
     'status is 200': () => res.status === 200,
   });
