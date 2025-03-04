1566f231536861814048bbb8151946e9e86627c9
TapanGL1 <tapan.pathy@globallogic.com>
2025-03-04 17:59:56 +0530

    Update post-400-status-test.ts
    New API request

--- src/post-400-status-test.ts
+++ src/post-400-status-test.ts
@@ -1,19 +1,16 @@
 import { sleep, check } from 'k6';
 import { Options } from 'k6/options';
-
-/* @ts-ignore */
-import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 import http from 'k6/http';
 
 export let options:Options = {
-  vus: 50,
-  duration: '10s'
+  vus: 1,
+  duration: '30s'
 };
 
 export default () => {
-  const res = http.post('https://httpbin.org/status/400');
+  const res = http.get('https://app.launchdarkly.com/sdk/evalx/5a7c7b5afd95332db01686a0/users/eyJrZXkiOiI3MjViNWE5Mi0zMTBiLTQ4YWUtYmMwNy00YjEyOWFkODhkMGQiLCJjdXN0b20iOnsiY2xhaW1zIjpbXSwiY3VycmVudFBsYW5JZCI6IjE4MDBjb250YWN0cyIsInBsYW5JZHMiOlsiMTgwMGNvbnRhY3RzIl19fQ');
   check(res, {
-    'status is 400': () => res.status === 400,
+    'status is 200': () => res.status === 200,
   });
-  sleep(randomIntBetween(1,5));
+  sleep(1);
 };
